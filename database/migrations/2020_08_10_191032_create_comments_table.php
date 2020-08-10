6be6aed7d7d3588440be5f423ca22ae9a79fdc2a
Nhala <zirconiump42@gmail.com>
2020-08-10 21:02:41 +0900

    migrationファイル作成

---
+++ database/migrations/2020_08_10_191032_create_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('report_id');
+            $table->unsignedBigInteger('user_id');
+            $table->text('comment');
+            $table->timestamps();
+            
+            $table->foreign('report_id')->references('id')->on('reports');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
--- database/migrations/2020_08_10_191044_create_contents_table.php
+++ database/migrations/2020_08_10_191044_create_contents_table.php
@@ -4,8 +4,12 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateContentsTable extends Migration
 {
+    /**
+     * contentsテーブルは、日誌の内容・本文そのものを表す
+     */
+    
     /**
      * Run the migrations.
      *
@@ -13,12 +17,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('contents', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('report_id');
+            $table->text('content');
             $table->timestamps();
+            
+            $table->foreign('report_id')->references('id')->on('reports');
         });
     }
 
@@ -29,6 +34,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('contents');
     }
 }
