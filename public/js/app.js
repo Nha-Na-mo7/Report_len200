092917bbfa761771654d99e9ef73a8983ec2ca6e
Nhala <zirconiump42@gmail.com>
2020-08-25 15:13:34 +0900

    フロントエンド : CSSをSASSに変更(FLOCSSを予定) ;

--- public/js/app.js
+++ public/js/app.js
@@ -1992,10 +1992,6 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-//
-//
-//
-//
 /* harmony default export */ __webpack_exports__["default"] = ({
   computed: {
     nowtime: function nowtime() {
--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -1,9 +1,5 @@
 <template>
   <footer class="footer">
-<!--    <button class="button button&#45;&#45;link"></button>-->
-<!--    <RouterLink class="button button&#45;&#45;link" to="/login">-->
-<!--      Footer - ログイン / 新規登録-->
-<!--    </RouterLink>-->
     <div>
       <h3>{{ nowtime }} ... リロード時刻</h3>
       <span>©️ Copyright 2020</span>
