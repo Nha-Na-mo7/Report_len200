936bdfb410351b60b47ad2356a6f178813eafbd2
Nhala <zirconiump42@gmail.com>
2020-08-31 10:58:41 +0900

    Report.vue　時刻フォーマットの表示修正

--- resources/js/pages/reports/Report.vue
+++ resources/js/pages/reports/Report.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="reports">
     <div class="reports__main">
-      <span class="reports__date">{{ item.created_at }}</span>
+      <span class="reports__date">{{ item.created_at | moment }}</span>
       <span class="reports__username">{{ item.owner.name }}</span>
     </div>
     <h2 class="reports__title">{{ item.report_title }}</h2>
@@ -20,11 +20,19 @@
 </template>
 
 <script>
+// DBのcreated_atを任意の日付フォーマットに変更するライブラリ
+import moment from 'moment';
+
 export default {
   props: {
     item: {
       type: Object,
       required: true
+    }
+  },
+  filters: {
+    moment: function (date) {
+      return moment(date).format('YY/M/D HH:mm')
     }
   }
 }
