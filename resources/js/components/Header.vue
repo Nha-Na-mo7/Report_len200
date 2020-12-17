faef6cd1a04427b373ff66692d775a8e4a991bf2
Nhala <zirconiump42@gmail.com>
2020-12-17 20:15:43 +0900

    改行とクラスのふよ

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -4,14 +4,25 @@
     <div class="navbar__menu">
       <!--リンク先が変わらない場合はtoでリンクを指定する-->
       <!--動的にリンク先が変わる場合、:toでリンクを指定する-->
-      <RouterLink class="btn btn--link" to="/reports/new" v-if="isLogin">新規作成</RouterLink>
+      <RouterLink
+          class="btn btn--link c-btn c-btn--link"
+          to="/reports/new"
+          v-if="isLogin"
+      >新規作成</RouterLink>
       <RouterLink class="btn btn--link" :to="`/mypage/${this.user_id}`" v-if="username">{{ username }}</RouterLink>
       <div class="navbar__item" v-else>
-        <RouterLink class="btn btn--link" to="/login">ログイン/新規登録</RouterLink>
+        <RouterLink
+            class="btn btn--link c-btn c-btn--link"
+            to="/login"
+        >ログイン/新規登録</RouterLink>
       </div>
 
       <!--ログアウトボタン-->
-      <button class="btn btn--link" v-on:click="logout" v-if="isLogin">ログアウトする</button>
+      <button
+          class="btn btn--link c-btn c-btn--link"
+          v-on:click="logout"
+          v-if="isLogin"
+      >ログアウトする</button>
 
     </div>
   </nav>
