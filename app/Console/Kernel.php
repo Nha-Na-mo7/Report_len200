b3de38c9ac082677ed57a899048ab9ec472b3957
Nhala <zirconiump42@gmail.com>
2021-01-01 00:10:51 +0900

    マイグレーションにコメント付与

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,6 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        // スケジュール更新の時ここに記述をする
     }
 
     /**
